b9089f205b8a4c985590ab5519de5efefb373792
yakintech <cagatayyildiz87@gmail.com>
2021-12-07 15:02:48 +0300

    react router

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
+
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Product'>
+
+
+
+        <Stack.Screen name='Product' component={ProductsScreen} />
+        <Stack.Screen name='ProductDetail' component={ProductDetail} />
+
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home Screen' }} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
@@ -18,4 +37,83 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listItem: {
+    padding: 15,
+    margin: 2,
+    backgroundColor: 'tomato'
+  }
 });
+
+
+
+const ProductsScreen = ({ navigation }) => {
+
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+
+
+    fetch('https://northwind.vercel.app/api/products')
+      .then(res => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+
+  }, [])
+
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => navigation.navigate('ProductDetail', { id: item.id, price: item.unitPrice })}>
+
+      <Text style={{ padding: 6, backgroundColor: 'aqua', margin: 3 }}>{item.name}</Text>
+
+    </TouchableOpacity>
+
+  )
+
+  return (
+    <FlatList
+      data={products}
+      renderItem={renderItem}
+    >
+
+    </FlatList>
+  )
+
+
+}
+
+
+const ProductDetail = ({ navigation, route }) => {
+
+
+  const { id, price } = route.params
+
+  return (<Text>Product Detail Screen - {id} - {price}</Text>)
+
+}
+
+
+const HomeScreen = ({ navigation }) => {
+  return (
+
+    <>
+      <Text>Home Screen</Text>
+      <Button title='Go to detail screen' onPress={() => navigation.navigate('Detail')}></Button>
+    </>
+
+
+
+
+  )
+}
+
+
+const DetailScreen = ({ navigation }) => {
+  return (<Text>Detail Screen</Text>)
+}
+
+
+
+//BrowserRouter => Routes => Route
